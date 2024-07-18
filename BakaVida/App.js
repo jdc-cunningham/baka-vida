0728ab6ae2fb4caa06839faa0fd69fa2e3661ee2
Jacob Cunningham <jcun4128@gmail.com>
2024-07-17 19:29:41 -0500

    build basic app

---
+++ BakaVida/App.js
@@ -0,0 +1,154 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const date = new Date();
+  const currentDay = date.getDay();
+
+  const amazonWorkday = {
+    what: 'Amazon Warehouse',
+    duration: 11,
+    units: 'hours',
+    value: 18,
+    type: 'full time',
+    hourRange: [6, 17],
+  };
+
+  const deliveries = {
+    what: 'DD/UE',
+    duration: 5,
+    type: 'gig',
+    value: 20, // a good day
+    hourRange: [17, 24],
+  };
+
+  const plDono = {
+    what: 'Plasma Donation',
+    duration: 2,
+    type: 'gig',
+    value: 60,
+    hourRange: [17, 19],
+  };
+
+  const dayValues = {
+    Sunday: [amazonWorkday],
+    Monday: [amazonWorkday, plDono, deliveries],
+    Tuesday: [amazonWorkday, deliveries],
+    Wednesday: [amazonWorkday],
+    Thursday: [amazonWorkday, deliveries],
+    Friday: [amazonWorkday, plDono, deliveries],
+    Saturday: [amazonWorkday, deliveries],
+  };
+
+  const today = days[currentDay];
+
+  const currentHour = date.getHours();
+
+  const inRange = job => currentHour <= job.hourRange[1];
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title={today}>Jobs for today:</Section>
+          {dayValues[today].map((incomeSource, index) => (
+            inRange(incomeSource) ? <Section title='Job' key={index}>
+              <Text style={styles.highlight}>{incomeSource.what}</Text>
+                {'\n'}
+              From {incomeSource.hourRange[0]} to {incomeSource.hourRange[1]}
+              {'\n'}
+              Expected: ${incomeSource.value * (incomeSource.hourRange[1] - incomeSource.hourRange[0])}.00
+              </Section>
+            : null
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,5 @@
+### Baka Vida (idiot life)
+
+This will drive my life for a while.
+
+It's pretty much a calendar app for Android.
